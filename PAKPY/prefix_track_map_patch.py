620df4593f427df44ea49e8caf1049a61a13f759
waxed <119123752+waxedhyphen@users.noreply.github.com>
2026-07-16 23:48:20 +0200

    Install normal clip structure parser

--- PAKPY/prefix_track_map_patch.py
+++ PAKPY/prefix_track_map_patch.py
@@ -1,3 +1,4 @@
+import anim_normal_clip_structure_patch as n
 import anim_packed_sample_decode as p
 import skel_full_hierarchy_patch as s
 
@@ -5,3 +6,4 @@ import skel_full_hierarchy_patch as s
 def install(App):
     s.install_into()
     p.install_into()
+    n.install_into()
