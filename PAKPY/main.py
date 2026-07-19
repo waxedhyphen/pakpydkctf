eb813b7dd30da9fcba6844281a67a60159b0680b
waxed <119123752+waxedhyphen@users.noreply.github.com>
2026-07-19 20:37:17 +0200

    Install exact SKEL rig preservation after legacy export hooks

--- PAKPY/main.py
+++ PAKPY/main.py
@@ -7,6 +7,7 @@ from char_skeletal_package_patch import install as install_char_skeletal_package
 from uuid_gui_patch import install as install_uuid_gui
 from missing_txtr_export_error_patch import install as install_missing_txtr_export_error
 from skeletal_tail_patch import install as install_skeletal_tail_patch
+from exact_skeletal_rig_patch import install as install_exact_skeletal_rig_patch
 from tree_ctrl_select_patch import install as install_tree_ctrl_select_patch
 from model_animation_refs_patch import install as install_model_animation_refs_patch
 from char_anim_selector_patch import install as install_char_anim_selector_patch
@@ -27,6 +28,7 @@ from anim_normal_clip_bind_patch import install as install_anim_normal_clip_bind
 from group_accept_patch import install as install_group_accept_patch
 from windows_compat import configure_root, setup_windows_process
 install_skeletal_tail_patch()
+install_exact_skeletal_rig_patch()
 install_dcln_gui(App)
 install_room_gui(App)
 install_char_gui(App)
